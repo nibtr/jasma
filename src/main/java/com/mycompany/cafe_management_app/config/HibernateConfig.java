49f20536a651d793e44df1b8005eaa4d2ea64a29
Hiếu Nguyễn <74890715+NoobProgammer@users.noreply.github.com>
2023-02-24 17:40:28 +0700

    feature: add hibernate config file and depedency

---
+++ src/main/java/com/mycompany/cafe_management_app/config/HibernateConfig.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cafe_management_app.config;
+
+import com.mycompany.cafe_management_app.model.Account;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ *
+ * @author Hieu
+ */
+public class HibernateConfig {
+    private static SessionFactory sessionFactory;
+    
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                
+//                Configure the properties
+                configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+                configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydatabase");
+                configuration.setProperty("hibernate.connection.username", "root");
+                configuration.setProperty("hibernate.connection.password", "root");
+                configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+                configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+                
+//                Add new POJOs here
+                configuration.addAnnotatedClass(Account.class);
+                
+//                Build the service factory
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+                
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+  
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return sessionFactory;
+    }
+}
