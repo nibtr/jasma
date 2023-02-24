02eafecc8431cdd2c457a1bee34a70a04f0b5765
Hiếu Nguyễn <74890715+NoobProgammer@users.noreply.github.com>
2023-02-24 09:29:54 +0700

    init: boilerplate

---
+++ CafeManagementApp/src/main/java/com/mycompany/cafemanagementapp/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cafemanagementapp;
+
+/**
+ *
+ * @author Hieu
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ CafeManagementApp/src/main/java/com/mycompany/cafemanagementapp/controller/LoginController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cafemanagementapp.controller;
+
+/**
+ *
+ * @author Hieu
+ */
+public class LoginController {
+    
+}
